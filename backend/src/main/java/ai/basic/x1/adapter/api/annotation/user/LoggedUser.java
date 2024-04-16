5fa664cfaa025687127d238554580704cb28b6d6
Karthik Sundar <46419602+vickyskarthik@users.noreply.github.com>
2024-04-16 23:10:52 +0530

    Update LoggedUser.java with comments

--- backend/src/main/java/ai/basic/x1/adapter/api/annotation/user/LoggedUser.java
+++ backend/src/main/java/ai/basic/x1/adapter/api/annotation/user/LoggedUser.java
@@ -8,9 +8,16 @@ import java.lang.annotation.Target;
 /**
  * @author andy
  */
-@Target({ElementType.PARAMETER})
-@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER})    // Specifies that this annotation can only be applied to method parameters.
+@Retention(RetentionPolicy.RUNTIME) // Specifies that this annotation will be available at runtime for reflection.
 public @interface LoggedUser {
+     /**
+     * Defines whether the logged-in user information is required by default.
+     * If true, the method where this is applied will expect a user to be authenticated.
+     * If false, the method can be executed even without user authentication.
+     *
+     * @return true if the user details are required, false otherwise.
+     */
     boolean required() default true;
 }
 
